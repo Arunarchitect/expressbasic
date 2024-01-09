3b0828c2ef5c25395a92a166866e1fb0528f72c5
Arunarchitect <intformbase@gmail.com>
2024-01-09 10:56:25 +0530

    added download function

--- app.js
+++ app.js
@@ -5,36 +5,16 @@ const path = require('path');
 const port = 5000;
 
 
-app.use(express.static('public'))
-
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'index.html'));
-})
-
 //Product
-app.get('/product/:category/:brand', (req,res)=>{
-    const {category} = req.params;
-    const {brand} = req.params;
-
-    res.send(`Product: ${category} and Brand:${brand}`);
-     
-})
-
-//Product
-app.get('/product', (req,res)=>{
+app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, 'product.html'));
 })
 
-
-//About
-app.get('/about', (req,res)=>{
-    res.sendFile(path.join(__dirname, 'about.html'));
+app.get('/file', (req,res)=>{
+    res.download(path.join(__dirname, 'sample.txt'));
 })
 
-//Home
-app.get('/contact', (req,res)=>{
-    res.sendFile(path.join(__dirname, 'contact.html'));
-})
+
 
 app.listen(port, () => {
     console.log(`App listening on Port ${port}`)
